1ff26dde9bc519efc0f34a9d6802cf80fc7ee39f
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2021-09-09 15:42:23 -0300

    inicio do projeto

---
+++ AppCounter/AppCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCounterApp.swift
+//  AppCounter
+//
+//  Created by João Alexandre on 09/09/21.
+//
+
+import SwiftUI
+
+@main
+struct AppCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppCounter/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  AppCounter
+//
+//  Created by João Alexandre on 09/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, IOS!")
+            .background(Color.blue)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
